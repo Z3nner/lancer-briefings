d1cec103f68d22212093bc7b0070e33e27ad19c9
Z3nner <76132631+Z3nner@users.noreply.github.com>
2022-02-17 23:20:35 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -139,14 +139,6 @@ export default {
           "frame": "Caliban",
           "mech": "A Little Shock"
         },
-        {
-          "callsign": "X",
-          "alias": "Xerath Dinis VI",
-          "code": "2f07437b-adb5-4ff0-bc98-0b12a9ff0c29//99b02e0f-0dc4-4e9e-b29d-7269d9e1c5ea",
-          "corpro": "IPS-N",
-          "frame": "Lancaster",
-          "mech": "Merely Adrift"
-        },
       ],
       "header": {
         "planet": "KIEMENE",
